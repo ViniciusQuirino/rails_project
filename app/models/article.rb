b0fc4b6c39ebc9351e1701629ebcf466ee5babfa
ViniciusQuirino <viniciusquirino13@hotmail.com>
2025-04-29 15:33:08 -0300

    Editar Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 } # campo title obrigatório
+  validates :body, presence: true, length: { minimum: 10 } # campo body obrigatório
 end
